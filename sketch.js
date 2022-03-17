928b5dacbceddca60167b3c8258c8c857ad808da
catalina1699 <101141376+catalina1699@users.noreply.github.com>
2022-03-17 17:15:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var button
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(random(0,255), random(0,255), random(0,255));
+    button = createButton('hii');
+  button.position(500, 500);
+  button.mousePressed(salvar)
+  frameRate (10)
+ 
+}
+
+function draw() {
+  fill(random(0,255))
+  circle(mouseX, mouseY, 40);
+describe('black circle with white outline in mid of gray canvas')
+
+}
+
+function salvar (){
+  saveCanvas ("imagen","jpg")
+}
